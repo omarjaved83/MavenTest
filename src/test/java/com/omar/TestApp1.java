321c5918307de5be66905505fec4f54271dbaea8
omarjaved83 <omarjaved83@outlook.com>
2018-09-07 08:53:15 +0200

    Update TestApp1.java

--- src/test/java/com/omar/TestApp1.java
+++ src/test/java/com/omar/TestApp1.java
@@ -183,7 +183,7 @@ public class TestApp1 extends TestCase {
 	}catch(Exception ex){}
 	TestCase.assertEquals(App.getHelloWorld(), "Hello World");	
 	}
-*/	
+	
 	@Test
 	public void testUnsafeIterator(){
 	
@@ -206,7 +206,7 @@ public class TestApp1 extends TestCase {
 		
 	TestCase.assertEquals(App.getHelloWorld(), "Hello World");
 	}	
-/*		
+		
 	@Test
 	public void testUnsafeMapIterator(){
 	
@@ -231,6 +231,23 @@ public class TestApp1 extends TestCase {
 		
 	//TestCase.assertEquals(App.getHelloWorld(), "Hello World");
 	}		
-*/	
+*/
+	
+	@Test
+	public void testLambda(){
+		List<String> myList =
+    		Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+		myList
+		    .stream()
+		    .filter(s -> s.startsWith("c"))
+		    .map(String::toUpperCase)
+		    .sorted()
+		    .forEach(System.out::println);
+	
+	
+	}
+	
+	
 }
 
